7d9aaa941dad1a726f398323f91b4ae3e2514da0
lisa367 <75017410+lisa367@users.noreply.github.com>
2023-01-19 23:53:12 +0100

    Projet 2 - phase 1

--- Projet 2/projet_2_script.py
+++ Projet 2/projet_2_script.py
@@ -1,2 +1,58 @@
-import requests
-import bs4
+import requests as rq
+from bs4 import BeautifulSoup as bs
+from datetime import datetime as dt
+from pathlib import Path
+
+
+todays_date = dt.today()
+extraction_date = todays_date.strftime("%Y-%m-%d_%H%M%S")
+
+data_directory = Path.cwd() / "data"
+images_directory = data_directory / "images"
+
+#data_directory.mkdir(exist_ok=True)
+#images_directory.mkdir(exist_ok=True)
+
+
+###          PHASE 1            ###
+
+def request_and_parse(url):
+    response = rq.get(url).text
+    soup = bs(response, "html.parser")
+
+    return soup
+
+
+def get_book_info(book_url):
+    soup = request_and_parse(book_url)
+    table_data = soup.select("tr")
+
+    upc = table_data[0]
+    title = soup.select("h1")
+    category = soup.select(".breadcrums > li:nth-of-type(3)")
+    price_including_tax = table_data[1].strip("£")
+    price_excluding_tax = table_data[2].strip("£")
+    available_stock = table_data[4][0]
+    star_rating = soup.select(".star-rating")["class"]
+    product_page_url = book_url
+    image_url = soup.select(".item, .active > img")
+    product_description = soup.select("#product_description > p")
+
+    if "One" in star_rating:
+        review_rating = 1
+    elif "Two" in star_rating:
+        review_rating = 2
+    elif "Three" in star_rating:
+        review_rating = 3
+    elif "Four" in star_rating:
+        review_rating = 4
+    elif "Five" in star_rating:
+        review_rating = 5
+
+    book_info = {"upc": upc, "title": title, "category": category, "price_including_tax": price_including_tax, "price_excluding_tax": price_excluding_tax, "available_stock": available_stock, "review_rating": review_rating, "product_page_url": product_page_url, "image_url": image_url, "product_description": product_description}
+
+    return book_info
+
+
+print(extraction_date)
+print(get_book_info("https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"))
