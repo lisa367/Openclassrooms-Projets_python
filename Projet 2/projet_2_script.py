4b1211ddacefa40f9d2a1e2b67eab33fbb736340
lisa367 <75017410+lisa367@users.noreply.github.com>
2023-01-20 23:59:11 +0100

    Projet 2 - phase 1

--- Projet 2/projet_2_script.py
+++ Projet 2/projet_2_script.py
@@ -27,18 +27,18 @@ def get_book_info(book_url):
     soup = request_and_parse(book_url)
     table_data = [bs(str(td), "html.parser").get_text() for td in soup.select("td")]
 
-    ''' 
     upc = table_data[0]
-    title = soup.select("h1").get_text()
-    category = soup.select(".breadcrums > li:nth-of-type(3)").get_text()
+    title = soup.select("h1")
+    category = soup.select(".breadcrums > li:nth-of-type(3)")
     price_including_tax = table_data[1].strip("Â£")
     price_excluding_tax = table_data[2].strip("Â£")
     available_stock = table_data[4][0]
-    star_rating = soup.select(".star-rating")["class"]
+    star_rating = soup.select(".star-rating")
     product_page_url = book_url
     image_url = soup.select(".item, .active > img")
     product_description = soup.select("#product_description > p")
 
+    '''
     if "One" in star_rating:
         review_rating = 1
     elif "Two" in star_rating:
@@ -49,11 +49,12 @@ def get_book_info(book_url):
         review_rating = 4
     elif "Five" in star_rating:
         review_rating = 5
+    '''
 
-    book_info = {"upc": upc, "title": title, "category": category, "price_including_tax": price_including_tax, "price_excluding_tax": price_excluding_tax, "available_stock": available_stock, "review_rating": review_rating, "product_page_url": product_page_url, "image_url": image_url, "product_description": product_description}
+    book_info = {"upc": upc, "title": title, "category": category, "price_including_tax": price_including_tax, "price_excluding_tax": price_excluding_tax, "available_stock": available_stock, "review_rating": 'review_rating', "product_page_url": product_page_url, "image_url": image_url, "product_description": product_description}
 
     return book_info
-    '''
+
     return table_data
 
 
