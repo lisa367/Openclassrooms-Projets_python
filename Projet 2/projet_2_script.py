8514f9a0a2750813c00ca7e54a2b0271d1ea7f78
lisa367 <75017410+lisa367@users.noreply.github.com>
2023-01-23 12:24:11 +0100

    Projet 2 - phase 1

--- Projet 2/projet_2_script.py
+++ Projet 2/projet_2_script.py
@@ -20,43 +20,54 @@ landing_page_url = "https://books.toscrape.com/"
 
 def request_and_parse(url):
     response = rq.get(url).text
-    soup = bs(response, "html.parser")
+    soup = bs(response, "html.parser") if response.ok else 'error'
 
     return soup
 
 
 def get_book_info(book_url):
     soup = request_and_parse(book_url)
-    # table_data = [bs(str(td), "html.parser").get_text() for td in soup.select("td")]
-    table_data = [td.contents[0] for td in soup.select("td")]
+    if soup != 'error':
+        table_data = [td.contents[0] for td in soup.select("td")]
 
-    upc = table_data[0]
-    title = soup.select("h1")[0].contents[0]
-    category = soup.select(".breadcrumb li:nth-of-type(3) a")[0].contents[0]
-    price_including_tax = table_data[2].strip("Â£")
-    price_excluding_tax = table_data[3].strip("Â£")
-    available_stock = table_data[5].strip("In stock (").strip(" available)")
-    star_rating = soup.select(".star-rating")[0]['class']
-    product_page_url = book_url
-    image_url = f"{landing_page_url}{soup.select('.carousel-inner > div > img')[0]['src'].strip('../..')}"
-    product_description = soup.select("#product_description ~ p")[0].contents[0]
+        upc = table_data[0]
+        title = soup.select("h1")[0].contents[0]
+        category = soup.select(".breadcrumb li:nth-of-type(3) a")[0].contents[0]
+        price_including_tax = table_data[2].strip("Â£")
+        price_excluding_tax = table_data[3].strip("Â£")
+        available_stock = table_data[5].strip("In stock (").strip(" available)")
+        star_rating = soup.select(".star-rating")[0]['class']
+        product_page_url = book_url
+        image_url = f"{landing_page_url}{soup.select('.carousel-inner > div > img')[0]['src'].strip('../..')}"
+        product_description = soup.select("#product_description ~ p")[0].contents[0]
 
+        if "One" in star_rating:
+            review_rating = 1
+        elif "Two" in star_rating:
+            review_rating = 2
+        elif "Three" in star_rating:
+            review_rating = 3
+        elif "Four" in star_rating:
+            review_rating = 4
+        elif "Five" in star_rating:
+            review_rating = 5
 
-    if "One" in star_rating:
-        review_rating = 1
-    elif "Two" in star_rating:
-        review_rating = 2
-    elif "Three" in star_rating:
-        review_rating = 3
-    elif "Four" in star_rating:
-        review_rating = 4
-    elif "Five" in star_rating:
-        review_rating = 5
-        
+        book_info = {"upc": upc, "title": title, "category": category, "price_including_tax": price_including_tax, "price_excluding_tax": price_excluding_tax, "available_stock": available_stock, "review_rating": review_rating, "product_page_url": product_page_url, "image_url": image_url, "product_description": product_description}
 
-    book_info = {"upc": upc, "title": title, "category": category, "price_including_tax": price_including_tax, "price_excluding_tax": price_excluding_tax, "available_stock": available_stock, "review_rating": review_rating, "product_page_url": product_page_url, "image_url": image_url, "product_description": product_description}
+        return book_info
 
-    return book_info
+    else:
+        return "La requête n'a pu aboutir."
 
 # print(extraction_date)
 print(get_book_info("https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"))
+
+def create_image_file(image_url, book_title):
+    image_file_name = f"{'_'.join(book_title.lower().split(' '))}.jpg"
+    #print(image_file_name)
+    image_file_path = f"{images_directory} / {image_file_name}"
+    if not image_file_path.exists():
+        with open(image_file_path, 'wb') as image_file:
+            r = rq.get(image_url, stream=True)
+            if r.ok:
+                image_file.write(r.content)
