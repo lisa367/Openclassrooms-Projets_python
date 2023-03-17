4aeabcb2ed9f4616b6c9294786a579ed7482ea65
lisa367 <75017410+lisa367@users.noreply.github.com>
2023-03-17 13:29:10 +0100

    classes du controleur

--- Projet 4/controleur.py
+++ Projet 4/controleur.py
@@ -10,9 +10,31 @@ option = instance_joueur.choix_option()
 
 if option == "ajouter":
     ex = instance_joueur.ajouter()
-if option == "modifier":
+elif option == "modifier":
     ex = instance_joueur.modifier()
-if option == "supprimer":
+elif option == "supprimer":
     ex = instance_joueur.supprimer()
 
 print(ex)
+
+class JoueurMenu:
+    def __init__(self) -> None:
+        self.option_choisie = ""
+        self.labels = JoueurModel.headers
+        self.instance_joueur = ""
+
+    def instance_joueur(self):
+        self.instance_joueur = JoueurView(labels=self.labels, menu_choisi=menu)
+        return self.instance_joueur
+    
+    def choix_option(self):
+        self.option_choisie = self.instance_joueur.choix_option()
+        return self.option_choisie
+    
+    def instruction(self):
+        if option == "ajouter":
+            self.instance_joueur.ajouter()
+        elif option == "modifier":
+            self.instance_joueur.modifier()
+        elif option == "supprimer":
+            self.instance_joueur.supprimer()
