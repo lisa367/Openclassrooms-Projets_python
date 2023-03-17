fe25d814911b92cde1f8684f37addcf26e4213c9
lisa367 <75017410+lisa367@users.noreply.github.com>
2023-03-17 12:03:47 +0100

    classes du controleur

--- Projet 4/controleur.py
+++ Projet 4/controleur.py
@@ -2,6 +2,16 @@ from modele import JoueurModel
 from vue import View, JoueurView
 
 
-menu = View.choix_menu()
-joueur = JoueurView(labels=JoueurModel.headers, menu_choisi=menu)
-option = joueur.choix_option()
+instance_vue = View()
+menu = instance_vue.choix_menu()
+instance_joueur = JoueurView(labels=JoueurModel.headers, menu_choisi=menu)
+option = instance_joueur.choix_option()
+
+if option == "ajouter":
+    ex = instance_joueur.ajouter()
+if option == "modifier":
+    ex = instance_joueur.modifier()
+if option == "supprimer":
+    ex = instance_joueur.supprimer()
+
+print(ex)
