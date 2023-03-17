3d5a92412e79d1a514732671db9d0bbeceefe67c
lisa367 <75017410+lisa367@users.noreply.github.com>
2023-03-17 11:44:40 +0100

    classes du controleur

--- Projet 4/controleur.py
+++ Projet 4/controleur.py
@@ -1,3 +1,7 @@
 from modele import JoueurModel
-from vue import JoueurModel
+from vue import View, JoueurView
 
+
+menu = View.choix_menu()
+joueur = JoueurView(labels=JoueurModel.headers, menu_choisi=menu)
+option = joueur.choix_option()
