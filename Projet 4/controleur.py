dfe4edda128949e2ce8e288ca18f050f18f0c567
lisa367 <75017410+lisa367@users.noreply.github.com>
2023-03-18 21:53:44 +0100

    controleur - classe JoueurMenu

--- Projet 4/controleur.py
+++ Projet 4/controleur.py
@@ -4,24 +4,16 @@ from vue import View, JoueurView
 
 instance_vue = View()
 menu = instance_vue.choix_menu()
-instance_joueur = JoueurView(labels=JoueurModel.headers, menu_choisi=menu)
-option = instance_joueur.choix_option()
+# instance_joueur = JoueurView(labels=JoueurModel.headers, menu_choisi=menu)
+# option = instance_joueur.choix_option()
 # print(option)
-
-if option == "ajouter":
-    ex = instance_joueur.ajouter()
-elif option == "modifier":
-    ex = instance_joueur.modifier()
-elif option == "supprimer":
-    ex = instance_joueur.supprimer()
-
-print(ex)
 
 class JoueurMenu:
     def __init__(self) -> None:
         self.option_choisie = ""
         self.labels = JoueurModel.headers
         self.instance_joueur = ""
+        self.instance_modele = JoueurModel()
 
     def instance_joueur(self):
         self.instance_joueur = JoueurView(labels=self.labels, menu_choisi=menu)
@@ -36,14 +28,27 @@ class JoueurMenu:
             self.ajouter_db()
         elif self.option == "modifier":
             self.modifier_db()
-        elif option == "supprimer":
+        elif self.option == "supprimer":
             self.supprimer_db()
 
     def ajouter_db(self):
         new_entry = self.instance_joueur.ajouter()
+        self.instance_modele.enregistrer(new_entry=new_entry)
 
     def modifier_db(self):
         new_entry = self.instance_joueur.modifier()
+        self.instance_modele.modifier()
 
     def supprimer_db(self):
         new_entry = self.instance_joueur.supprimer()
+
+
+
+"""if option == "ajouter":
+    ex = instance_joueur.ajouter()
+elif option == "modifier":
+    ex = instance_joueur.modifier()
+elif option == "supprimer":
+    ex = instance_joueur.supprimer()
+
+print(ex)"""
