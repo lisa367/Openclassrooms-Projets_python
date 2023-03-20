88aaf8e92eed50d3951c7a08c431346f3647fffb
lisa367 <75017410+lisa367@users.noreply.github.com>
2023-03-20 14:03:05 +0100

    modified JoueurModel class

--- Projet 4/modele.py
+++ Projet 4/modele.py
@@ -17,46 +17,38 @@ class JoueurModel:
 
     def __init__(self):
         self.data_dict = {}
-        self.filter = "identifiant"
-        
-        """self.data_dict = data_dict
-        self.identifiant = data_dict.get("identifiant", "")
-        self.nom = data_dict.get("nom", "")
-        self.prenom = data_dict.get("prenom", "")
-        self.date_naissance = data_dict.get("nom", "")
-        self.nombre_matchs_total = 0
-        self.nombre_matchs_gagnes = 0
-        self.score_total = 0
-
-    def __repr__(self) -> str:
-        return f"joueur: {self.prenom} {self.nom} - {self.identifiant}" """
+        self.default_filter = "identifiant"
+        self.database = db_joueurs
     
-    def entry_already_exists(self, filter_value, filter="identifant"):
-        db = JoueurModel.database
-        id_check = db.search(query[filter] == filter_value)
+    def entry_already_exists(self, filter_value):
+        filter = self.default_filter
+        id_check = self.search(query[filter] == filter_value)
 
         return id_check
 
     def enregistrer(self, new_entry):
-        # new_entry = self.data_dict
-        JoueurModel.database.insert(new_entry)
-
+        self.database.insert(new_entry)
         return new_entry
 
-    def modifier(self, data_dict, id_value, filter="identifant"):
-        db = JoueurModel.database
-        # db.update({field: new_value}, query[filter] == value)
-        # db.update(data_dict, query[filter] == id_value)
-        db.upsert(data_dict, query[filter] == id_value)
+    def modifier(self, data_dict, id_value):
+        filter = self.default_filter
+        self.database.update(data_dict, query[filter] == id_value)
 
-    def supprimer(self, filter_value, filter="identifiant"):
-        db = JoueurModel.database
-        db.remove(query[filter] == filter_value)
+    def supprimer(self, filter_value):
+        filter = self.default_filter
+        self.database.remove(query[filter] == filter_value)
+
+    def retreive_all(self):
+        print(self.database.all())
 
 
-ex = {"identifiant": "CCCC", "nom": "Martin", "prenom": "Jean", "date_naissance": "02/04/2009"}
-to_modify = ["CCCC", {"prenom": "Julie", "date_naissance": "12/04/2011"}]
+# ex1 = {"identifiant": "CCCC", "nom": "Marin", "prenom": "Julie", "date_naissance": "12/08/2007"}
+# ex2 = {"identifiant": "DDDD", "nom": "Renault", "prenom": "Zoe", "date_naissance": "21/09/2011"}
+# to_modify = ["CCCC", {"nom": "Marin", "prenom": "Julie", "date_naissance": "12/04/2011"}]
 instance_modele = JoueurModel()
-# instance_modele.enregistrer(ex)
-instance_modele.supprimer(filter="prenom", filter_value="Julie")
+
+# instance_modele.enregistrer(ex1)
+# instance_modele.enregistrer(ex2)
+instance_modele.supprimer(filter_value="DDDD")
 # instance_modele.modifier(data_dict=to_modify[1], id_value=to_modify[0])
+# instance_modele.retreive_all()
