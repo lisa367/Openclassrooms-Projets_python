b3ff4104102bae7c4562c3cecd984204413b1488
lisa367 <75017410+lisa367@users.noreply.github.com>
2023-04-14 09:28:25 +0200

    modified JoueurMenu

--- Projet 4/controleur.py
+++ Projet 4/controleur.py
@@ -1,7 +1,7 @@
 from datetime import timedelta, datetime as dt
 
-from modele import JoueurModel, modele_joueur, Tour, modele_tournoi
-from vue import View, JoueurView
+from modele import JoueurModel, db_joueurs, TournoiModel,db_tournois, Tour
+from vue import View, JoueurView, TournoiView
 from base import BaseMenu
 
 
@@ -12,41 +12,14 @@ menu = instance_vue.choix_menu()
 # option = instance_joueur.choix_option()
 # print(option)
 
-class JoueurMenu:
-    def __init__(self) -> None:
-        self.option_choisie = ""
-        self.labels = JoueurModel.headers
-        self.instance_joueur = ""
-        self.instance_modele = JoueurModel()
+modele_joueur = JoueurModel(filter_name="identifiant", database_name=db_joueurs)
+vue_joueur = JoueurView(labels=JoueurModel.headers, menu_choisi=menu)
 
-    def instance_joueur(self):
-        self.instance_joueur = JoueurView(labels=self.labels, menu_choisi=menu)
-        return self.instance_joueur
-    
-    def choix_option(self):
-        self.option_choisie = self.instance_joueur.choix_option()
-        return self.option_choisie
-    
-    def instruction(self):
-        if self.option == "ajouter":
-            self.ajouter_db()
-        elif self.option == "modifier":
-            self.modifier_db()
-        elif self.option == "supprimer":
-            self.supprimer_db()
+modele_tournoi = TournoiModel(filter_name="name", database_name=db_tournois)
+vue_tournoi = TournoiView(labels=TournoiModel, menu_choisi=menu)
 
-    def ajouter_db(self):
-        new_entry = self.instance_joueur.ajouter()
-        self.instance_modele.enregistrer(new_entry=new_entry)
-
-    def modifier_db(self, filter, data):
-        new_entry = self.instance_joueur.modifier()
-        id=new_entry["filter"]
-        self.instance_modele.modifier(value=id)
-
-    def supprimer_db(self):
-        entry = self.instance_joueur.supprimer()
-        self.instance_modele.supprimer(filter="identifiant", value=entry)
+class JoueurMenu(BaseMenu):
+    pass
 
 
 
@@ -83,7 +56,7 @@ class TournoiMenu(BaseMenu):
 
 
 #instance_modele = JoueurModel(filter_name="identifiant", database_name=db_joueurs)
-instance_vue = JoueurView(labels=JoueurModel.headers, menu_choisi="joueur")
+# instance_vue = JoueurView(labels=JoueurModel.headers, menu_choisi="joueur")
 
-menu_test = BaseMenu(modele_objet=modele_joueur, vue_objet=instance_vue)
-menu_test.instruction()
+# menu_test = BaseMenu(modele_objet=modele_joueur, vue_objet=instance_vue)
+# menu_test.instruction()
