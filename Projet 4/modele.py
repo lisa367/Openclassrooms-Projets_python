7e0c075807c65691a0596e0703a7cbd1e4583740
lisa367 <75017410+lisa367@users.noreply.github.com>
2023-04-17 15:13:41 +0200

    JoueurModel - __repr__ method

--- Projet 4/modele.py
+++ Projet 4/modele.py
@@ -14,21 +14,34 @@ class JoueurModel(BaseModel):
     database = db_joueurs
     # headers = ["identifiant", "nom", "prenom", "date_naissance", "score_total", "matchs_total", "matchs_gagnes"]
     headers = ["identifiant", "nom", "prenom", "date_naissance"]
-    verbose = {"identifiant": "l'identifiant", "nom": "le nom", "prenom": "le prénom", "date_naissance": "la date de naissance", }
+    verbose = {
+        "identifiant": "l'identifiant",
+        "nom": "le nom",
+        "prenom": "le prénom",
+        "date_naissance": "la date de naissance",
+    }
 
-    def __str__(self, joueur):
+    def __repr__(self, joueur):
         return f"{joueur['identifiant']}. {joueur['prenom'].title()} {joueur['nom'].upper()}"
 
 
 class MatchModel(BaseModel):
     headers = ["joueur_1", "score_1", "joueur_2", "score_2"]
+
     def __init__(self, filter_name, database_name):
         super().__init__(filter_name, database_name)
 
 
 class TournoiModel(BaseModel):
     headers = ["nom", "lieu", "date_debut", "date_fin", "nombre_tours", "joueurs"]
-    verbose = {"nom": "le nom","lieu": "le lieu", "debut": "la date de début", "fin": "la date de fin", "joueurs": "la liste de joueurs"}
+    verbose = {
+        "nom": "le nom",
+        "lieu": "le lieu",
+        "debut": "la date de début",
+        "fin": "la date de fin",
+        "joueurs": "la liste de joueurs",
+    }
+
     def __init__(self, filter_name, database_name, num_tours=4):
         super().__init__(filter_name, database_name)
         self.liste_joueurs = []
@@ -44,37 +57,40 @@ class Tour:
         self.nom = f"Round {self.round}"
         self.liste_joueurs = liste_joueurs
         self.debut = dt.now().strftime("%d/%m/%Y_%H:%M")
-        self.fin = (dt.now() + timedelta(hours = 1)).strftime("%d/%m/%Y_%H:%M")
+        self.fin = (dt.now() + timedelta(hours=1)).strftime("%d/%m/%Y_%H:%M")
         self.matchs = []
         self.paires = paires_dict
         self.scores = scores_dict
         self.ranking = []
         self.tour_info = {}
 
-
     def rank(self):
         self.ranking = sorted(self.scores, key=lambda joueur: self.scores[joueur])
         return self.ranking
-    
+
     def generation_paires(self):
         liste_joueurs = self.liste_joueurs
         if self.round == 1:
             random.shuffle(liste_joueurs)
-            sublists = [set(liste_joueurs[i:i + 2]) for i in range(0, len(liste_joueurs), 2)]
+            sublists = [
+                set(liste_joueurs[i : i + 2]) for i in range(0, len(liste_joueurs), 2)
+            ]
             # self.paires += sublists
 
         else:
             ordered_players = [player for player in self.rank()]
             sublists = []
-            while len(ordered_players) > 0 :
-                if (set(self.paires) != set([paire for paire in combinations(self.liste_joueurs, 2)])) :
-                    for j in range(1, len( ordered_players)):
-                        new_paire = {ordered_players[0], ordered_players[0+j]}
+            while len(ordered_players) > 0:
+                if set(self.paires) != set(
+                    [paire for paire in combinations(self.liste_joueurs, 2)]
+                ):
+                    for j in range(1, len(ordered_players)):
+                        new_paire = {ordered_players[0], ordered_players[0 + j]}
                         if new_paire in self.paires:
                             continue
                         else:
                             sublists.append(new_paire)
-                            ordered_players.pop(0+j)
+                            ordered_players.pop(0 + j)
                             ordered_players.pop(0)
                             break
                 else:
@@ -82,7 +98,7 @@ class Tour:
 
         self.paires.extend(sublists)
         return sublists
-    
+
     def resultat_match(self, joueur_1, joueur_2):
         match = []
         seq = ["nul", "non_nul"]
@@ -95,17 +111,17 @@ class Tour:
 
         else:
             seq2 = [0, 1]
-            score_1 = seq2.pop(random.randint(0,1))
+            score_1 = seq2.pop(random.randint(0, 1))
             score_2 = seq2[0]
             match.append((joueur_1, score_1))
             match.append((joueur_2, score_2))
             self.scores[joueur_1] += score_1
             self.scores[joueur_2] += score_2
-        
-        self.matchs.append(match)  
+
+        self.matchs.append(match)
         # print(match)
         return match
-    
+
     def get_tour_info(self):
         self.tour_info["nom"] = self.nom
         self.tour_info["debut"] = self.debut
@@ -113,12 +129,11 @@ class Tour:
         self.tour_info["matchs"] = self.matchs
 
         return self.tour_info
-    
+
     def resultat_tour(self):
         paires = self.generation_paires()
         for paire in list(paires):
             self.resultat_match(paire[0], paire[1])
-
 
 
 # modele_joueur = JoueurModel(filter_name="identifiant", database_name=db_joueurs)
