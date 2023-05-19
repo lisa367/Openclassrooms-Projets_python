74d041597f28f85624acaeb83b4c625d2c292941
lisa367 <75017410+lisa367@users.noreply.github.com>
2023-05-20 01:14:05 +0200

    arrows event listener

--- Projet 6/script.js
+++ Projet 6/script.js
@@ -1,2 +1,12 @@
-var arrowLeft = document.getElementsByClassName("arrow-left")
-var arrowRight = document.getElementsByClassName("arrow-right")
+var arrowLeft = document.getElementsByClassName("arrow-left");
+var arrowRight = document.getElementsByClassName("arrow-right");
+
+var body = document.getElementsByTagName("body")
+
+var cadreImg = document.getElementsByClassName("cadre-img");
+
+function changeBackgroundColor() {
+    body.style.backgroundColor = "pink"
+};
+
+arrowRight.onclick = changeBackgroundColor();
