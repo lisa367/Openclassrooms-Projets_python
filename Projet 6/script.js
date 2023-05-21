838e670c15489330518838b69f9b88a0612dbb3e
lisa367 <75017410+lisa367@users.noreply.github.com>
2023-05-21 19:13:10 +0200

    script file import

--- Projet 6/script.js
+++ Projet 6/script.js
@@ -1,12 +1,27 @@
-var arrowLeft = document.getElementsByClassName("arrow-left");
-var arrowRight = document.getElementsByClassName("arrow-right");
+// document.getElementById("img1.1").style.backgroundColor = "blue";
+// document.querySelector("h1").innerHTML = "Hello";
+// document.querySelector("h1").style.color = "blue";
+const arrowLeft = document.getElementsByClassName("arrow-left");
+const arrowRight = document.getElementsByClassName("arrow-right");
 
-var body = document.getElementsByTagName("body")
+const body = document.getElementsByTagName("body");
 
-var cadreImg = document.getElementsByClassName("cadre-img");
+const cadreImg = document.getElementsByClassName("cadre-img");
 
 function changeBackgroundColor() {
-    body.style.backgroundColor = "pink"
-};
+    body.style.backgroundColor = "pink";
+}
 
 arrowRight.onclick = changeBackgroundColor();
+const btn = document.getElementsByTagName("button");
+
+function random(number) {
+    return Math.floor(Math.random() * (number + 1));
+}
+
+btn.addEventListener("click", () => {
+    const rndCol = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+    body.style.backgroundColor = rndCol;
+});
+
+arrowLeft.style.color = "red";
