71f9e729596af3068c662377ee9e376b0cfc6393
lisa367 <75017410+lisa367@users.noreply.github.com>
2023-05-22 10:40:50 +0200

    events testing

--- Projet 6/script.js
+++ Projet 6/script.js
@@ -1,12 +1,13 @@
 // document.getElementById("img1.1").style.backgroundColor = "blue";
 // document.querySelector("h1").innerHTML = "Hello";
 // document.querySelector("h1").style.color = "blue";
-const arrowLeft = document.querySelector(".arrow-left");
-const arrowRight = document.querySelector(".arrow-right");
+const arrowLeft = document.getElementsByClassName("arrow-left")[0];
+const arrowRight = document.querySelectorAll(".arrow-right")[0];
 
 const bdy = document.querySelector("body");
 const btn = document.getElementsByTagName("button");
 // bdy.style.backgroundColor = "blue";
+// btn.style.backgroundColor = "pink";
 
 
 const cadreImg = document.getElementsByClassName("cadre-img");
@@ -15,7 +16,7 @@ function changeBackgroundColor() {
     bdy.style.backgroundColor = "pink";
 }
 function revertBackgroundColor() {
-    bdy.style.backgroundColor = "pink";
+    bdy.style.backgroundColor = "black";
 }
 function random(number) {
     return Math.floor(Math.random() * (number + 1));
@@ -23,6 +24,7 @@ function random(number) {
 
 arrowRight.onclick = changeBackgroundColor;
 arrowLeft.onclick = revertBackgroundColor;
+arrowLeft.style.color = "red";
 
 
 btn.addEventListener("click", () => {
@@ -30,4 +32,3 @@ btn.addEventListener("click", () => {
     bdy.style.backgroundColor = rndCol;
 });
 
-arrowLeft.style.color = "red";
