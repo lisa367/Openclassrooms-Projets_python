b84b4942d49f4b0d6e4b782deb766d5f055c0ebc
lisa367 <75017410+lisa367@users.noreply.github.com>
2023-05-29 23:51:36 +0200

    api test

--- Projet 6/api.js
+++ Projet 6/api.js
@@ -1,7 +1,10 @@
 const apiUrl = "http://localhost:8000/api/v1/titles";
 let parameters = "";
 
-const response = await fetch(`${apiUrl}/${parameters}`);
+async function get_info(parameter, value) {
+    const response = await fetch(`${apiUrl}/${parameter}=${value}`);
+    return response
+}
 
 const imageFrame = document.getElementById("img1.1");
 imageFrame.src = '';
