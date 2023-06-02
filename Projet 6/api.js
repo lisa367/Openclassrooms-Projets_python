6c49946f8480ffaa02febd700998bbc331bf9d3f
lisa367 <75017410+lisa367@users.noreply.github.com>
2023-06-02 16:14:02 +0200

    api calls testing

--- Projet 6/api.js
+++ Projet 6/api.js
@@ -1,9 +1,47 @@
 const apiUrl = "http://localhost:8000/api/v1/titles";
-let parameters = "";
-// console.log("Hello"
+const apiTest = "https://jsonplaceholder.typicode.com/users"
+
+console.log("Hello");
+
+async function get_info(url) {
+    const responseRaw = await fetch(url);
+    if (responseRaw.ok === true) {
+        return responseRaw.json();
+    }
+    // else { const response404 = "Oops !"; return response404 }
+    throw new Error("Oops !")
+
+}
+
+const test = get_info(apiUrl);
+console.log(test);
+
+
+/*
+
+const r = fetch(apiUrl)
+    .then(r => r.json())
+    .then(body => console.log(body))
+
+async function get_info(url) {
+    const responseRaw = await fetch(url);
+    responseRaw.text()
+    return responseRaw.body
+    /*
+    if (responseRaw.ok === true) {
+        responseRaw.text();
+        return responseRaw.body;
+    }
+    // else { const response404 = "Oops !"; return response404 }
+    throw new Error("Oops !")
+
+}
+const test = get_info(apiUrl);
+console.log(test);
+
 
 async function get_info(parameter, value) {
-    const responseRaw = await fetch(`${apiUrl}/${parameter}=${value}`);
+    const responseRaw = await fetch(`${apiUrl}/?${parameter}=${value}`);
     if (responseRaw.ok === true) {
         const responseJSON = responseRaw.json();
         return response
@@ -14,6 +52,7 @@ async function get_info(parameter, value) {
 }
 const best_movie = get_info("genre", "Romance");
 console.log(best_movie);
+*/
 
 // const imageFrame = document.getElementById("img1.1");
 // imageFrame.src = '';
