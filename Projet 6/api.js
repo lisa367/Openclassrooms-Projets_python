43e89526b88828e63791043f8d13e293202e0139
lisa367 <75017410+lisa367@users.noreply.github.com>
2023-06-02 14:23:38 +0200

    api calls

--- Projet 6/api.js
+++ Projet 6/api.js
@@ -1,10 +1,20 @@
 const apiUrl = "http://localhost:8000/api/v1/titles";
 let parameters = "";
+// console.log("Hello"
 
 async function get_info(parameter, value) {
-    const response = await fetch(`${apiUrl}/${parameter}=${value}`);
-    return response
-}
+    const responseRaw = await fetch(`${apiUrl}/${parameter}=${value}`);
+    if (responseRaw.ok === true) {
+        const responseJSON = responseRaw.json();
+        return response
+    }
+    // else { const response404 = "Oops !"; return response404 }
+    throw new Error("Oops !")
 
-const imageFrame = document.getElementById("img1.1");
-imageFrame.src = '';
+}
+const best_movie = get_info("genre", "Romance");
+console.log(best_movie);
+
+// const imageFrame = document.getElementById("img1.1");
+// imageFrame.src = '';
+
