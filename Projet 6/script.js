9e01271f172d92f03f012e4eec3ae48e100caa53
lisa367 <75017410+lisa367@users.noreply.github.com>
2023-06-15 20:56:13 +0200

    toggle modal window visibility

--- Projet 6/script.js
+++ Projet 6/script.js
@@ -1,32 +1,14 @@
-// document.getElementById("img1.1").style.backgroundColor = "blue";
-// document.querySelector("h1").innerHTML = "Hello";
-// document.querySelector("h1").style.color = "blue";
 const arrowsLeft = document.querySelectorAll(".arrow-left");
 const arrowsRight = document.querySelectorAll(".arrow-right");
 
-const bdy = document.querySelector("body");
-const btn = document.getElementsByTagName("button")[0];
-// bdy.style.backgroundColor = "blue";
-// btn.style.backgroundColor = "pink";
 
-
-// const cadreImg = document.getElementsByClassName("cadre-img");
 const cadreImg = document.querySelectorAll(".cadre-img");
 const vignettes = document.querySelectorAll(".vignette");
 const modalWindow = document.getElementById("modal-window");
 const closeButton = document.getElementById("close-button");
 // const div = document.querySelector(".movies-images");
 
-
-function changeBackgroundColor() {
-    bdy.style.backgroundColor = "pink";
-}
-function revertBackgroundColor() {
-    bdy.style.backgroundColor = "black";
-}
-function random(number) {
-    return Math.floor(Math.random() * (number + 1));
-}
+// Horizontal scrolling
 function moveRight(i) {
     const originalX = cadreImg[i].getBoundingClientRect()["x"];
     let newX = originalX;
@@ -46,12 +28,9 @@ function moveLeft(i) {
     cadreImg[i].style.transition = "1.5s";
     cadreImg[i].style.transform = `translateX(-${newX + (5 * 12)}px)`;
     newX -= (5 * 16);
-    // console.log(ori
     // console.log(cadreImg[i].getBoundingClientRect())
 }
 
-// arrowsRight[0].onclick = moveRight;
-// arrowsLeft[0].onclick = moveLeft;
 for (let i of [0, 1, 2, 3]) {
     arrowsRight[i].onclick = function () {
         moveRight(i)
@@ -62,13 +41,6 @@ for (let i of [0, 1, 2, 3]) {
     };
 }
 
-
-
-function scrollLeft(arrow) {
-    const i = document.id;
-    const index = parseInt(i);
-
-}
 
 // Toggle modal window visibility
 function displayModalWindow() {
@@ -83,12 +55,33 @@ vignettes.forEach(element => {
 })
 
 /*
+// document.getElementById("img1.1").style.backgroundColor = "blue";
+// document.querySelector("h1").innerHTML = "Hello";
+// document.querySelector("h1").style.color = "blue";
+
+const bdy = document.querySelector("body");
+const btn = document.getElementsByTagName("button")[0];
+// bdy.style.backgroundColor = "blue";
+// btn.style.backgroundColor = "pink";
 arrowLeft.style.color = "red";
+
+function changeBackgroundColor() {
+    bdy.style.backgroundColor = "pink";
+}
+function revertBackgroundColor() {
+    bdy.style.backgroundColor = "black";
+}
+function random(number) {
+    return Math.floor(Math.random() * (number + 1));
+}
 
 btn.addEventListener("click", () => {
     const rndCol = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
     bdy.style.backgroundColor = rndCol;
 });
+// arrowsRight[0].onclick = moveRight;
+// arrowsLeft[0].onclick = moveLeft;
+
 function left(value, index, array) {
     arrowsLeft[index].onclick = revertBackgroundColor;
 }
@@ -98,34 +91,15 @@ function right(value, index, array) {
 arrowsLeft.forEach(element => {
     // element.onclick = revertBackgroundColor;
     console.log(element.innerHTML)
-});*/
-
-// arrowsRight.forEach(right);
-//console.log(arrowsLeft)
-/*
-arrowsLeft.forEach(element => {
-    element.onclick = revertBackgroundColor;
-});
-arrowsRight.forEach(element => {
-    element.onclick = changeBackgroundColor;
 });
 
-arrowsRight[0].onclick = function () {
-        //.forEach(element => {
-            element.style.transition = "1s";
-            element.style.transform = "translateX(-5rem)";})//
-            div.style.transition = "1s";
-            div.style.transform = "translateX(5rem)";
-    
-        };
-arrowsLeft[0].onclick = function () {
-        //vignettes.forEach(element => {
-            element.style.transition = "1s";
-            element.style.transform = "translateX(-5rem)";
-            })//
-            div.style.transition = "1s";
-            div.style.transform = "translateX(-5rem)";
-        };
-    }
+
+
+function scrollLeft(arrow) {
+    const i = document.id;
+    const index = parseInt(i);
+
+}
 
 */
+
